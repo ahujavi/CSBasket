21c6c3764fa9b445dfd644476159dbc58c6b8a1b
ahujavi <ahujavi@hotmail.com>
2017-05-08 10:54:07 +0100

    Update OrderService.java

--- src/main/java/com/cs/basket/service/OrderService.java
+++ src/main/java/com/cs/basket/service/OrderService.java
@@ -1,71 +1,32 @@
 package com.cs.basket.service;
 
 import com.cs.basket.exception.UnrecognisedItemException;
-import com.cs.basket.model.Item;
 import com.cs.basket.model.Order;
-import com.cs.basket.offers.BuyOneGetOneFreeOffer;
 import com.cs.basket.offers.Offer;
-import com.cs.basket.offers.ThreeForTwoOffer;
-import com.cs.basket.service.OrderService;
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Iterables;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.concurrent.atomic.LongAdder;
-import java.util.stream.Collectors;
-
-import static com.cs.basket.model.Item.LIME;
-import static com.cs.basket.model.Item.MELON;
+import java.util.List;
 
 /**
  * Created by ahuja on 5/5/2017.
  */
-@Component
-public class OrderServiceImpl implements OrderService {
-    //NOTE - For now, I have assumed that "all" offers will be pre-loaded. However, we might need to provide a
-    //mechanism to support the fact that offers are expected to change very frequently in the real world.
-    @Autowired
-    Collection<Offer> offers;
-
-    void setOffers(Collection<Offer> offers) {
-        this.offers = offers;
-    }
-
-    //NOTE - encapsulating the order creation logic in a separate service allows us to keep Order as a POJO.
-    @Override
-    public Order createOrder(List<String> items) throws UnrecognisedItemException {
-        //prepare a map of Item and its quantity.
-        Map<Item, Long> mapOfItems = Optional.ofNullable(items).orElse(new ArrayList<>()).stream()
-                .map(i -> Item.itemFor(i))
-                .collect(Collectors.groupingBy(v -> v, Collectors.counting()));
-
-        //calculate total price prior to any discounts
-        long totalOriginalPrice = mapOfItems.entrySet().stream().mapToLong(
-                item -> item.getKey().getPriceInPence() * item.getValue()).sum();
-
-        return new Order(mapOfItems, totalOriginalPrice);
-    }
+public interface OrderService {
+    /**
+     * @param items the items.
+     * @return an order for the supplied items
+     * @throws UnrecognisedItemException if any item is unrecognised.
+     */
+    Order createOrder(List<String> items) throws UnrecognisedItemException;
 
     /**
-     * NOTE - For now, I have assumed that "all" offers will be pre-loaded. However, this method gives us the
-     * opportunity to
-     * A) load offers dynamically (e.g. from a RDBMS)
-     * B) return only those offers which apply to the order
-     *
      * @param order identifies the order.
      * @return the list of offers applicable to the order.
      */
-    @Override
-    public List<Offer> getOffers(final Order order) {
-        return offers.stream().filter(offer -> offer.appliesTo(order)).collect(Collectors.toList());
-    }
+    List<Offer> getOffers(Order order);
 
-    @Override
-    public long calculateDiscount(Order order, List<Offer> offers) {
-        return offers.stream().mapToLong(offer -> offer.calculateDiscount(order)).sum();
-    }
-
+    /**
+     * @param order the order for which to calculate the discount
+     * @param offers the discount is calculated on the basis of the offers.
+     * @return the discount for the order based on the supplied offers.
+     */
+    long calculateDiscount(Order order, List<Offer> offers);
 }
