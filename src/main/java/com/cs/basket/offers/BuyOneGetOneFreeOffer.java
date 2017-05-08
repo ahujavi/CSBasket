fa3f3696ac5e9b733775f89ded99786ae9f18410
ahujavi <ahujavi@hotmail.com>
2017-05-08 11:10:19 +0100

    Update BuyOneGetOneFreeOffer.java

--- src/main/java/com/cs/basket/offers/BuyOneGetOneFreeOffer.java
+++ src/main/java/com/cs/basket/offers/BuyOneGetOneFreeOffer.java
@@ -1,42 +1,19 @@
 package com.cs.basket.offers;
 
 import com.cs.basket.model.Item;
-import com.cs.basket.model.Order;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.concurrent.atomic.LongAdder;
 import java.util.function.BiFunction;
 
 import static com.cs.basket.model.Item.MELON;
 
 @Component
-public class BuyOneGetOneFreeOffer implements Offer {
-    //NOTE - in real world, the list of participating items will be loaded from a dynamic store (e.g. RDBMS).
-    final Item[] participatingItems = new Item[]{MELON};
+public class BuyOneGetOneFreeOffer extends Offer {
 
-    BiFunction<Long, Long, Long> buyOneGetOneFree = (itemQuantity, itemPrice) -> itemPrice * (itemQuantity/2);
+    static final BiFunction<Long, Long, Long> buyOneGetOneFree = (itemQuantity, itemPrice) -> itemPrice * (itemQuantity/2);
 
-    @Override
-    public long calculateDiscount(Order order) {
-        LongAdder discount = new LongAdder();
-        Arrays.stream(participatingItems)
-                .filter(itemOnOffer -> order.hasItem(itemOnOffer))
-                .forEach(itemOnOffer ->
-                        discount.add(buyOneGetOneFree.apply(order.getItemQuantity(itemOnOffer), itemOnOffer.getPriceInPence())));
-
-
-        return discount.longValue();
+    public BuyOneGetOneFreeOffer() {
+        super(new Item[]{MELON}, buyOneGetOneFree);
     }
 
-    @Override
-    public boolean appliesTo(Order order) {
-        for (Item itemOnOffer : participatingItems) {
-            if (order.hasItem(itemOnOffer)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
