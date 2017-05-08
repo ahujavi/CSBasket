eebcca88e620c0ef588d2e440c475d15980eb9d1
ahujavi <ahujavi@hotmail.com>
2017-05-08 11:13:59 +0100

    Update Offer.java

--- src/main/java/com/cs/basket/offers/Offer.java
+++ src/main/java/com/cs/basket/offers/Offer.java
@@ -13,27 +13,27 @@ public class Offer {
     //NOTE - in real world, the list of participating items will be loaded from a dynamic store (e.g. RDBMS).
     final Item[] participatingItems;
 
-    final BiFunction<Long, Long, Long> longBiFunction;
+    final BiFunction<Long, Long, Long> offerFunction;
 
-    public Offer(Item[] participatingItems, BiFunction<Long, Long, Long> longBiFunction) {
+    public Offer(Item[] participatingItems, BiFunction<Long, Long, Long> offerFunction) {
         this.participatingItems = participatingItems;
-        this.longBiFunction = longBiFunction;
+        this.offerFunction = offerFunction;
     }
 
     /**
-     * @param order the order on which to apply the longBiFunction.
-     * @return the discount if the supplied order qualifies for this longBiFunction. Else 0
+     * @param order the order on which to apply the offerFunction.
+     * @return the discount if the supplied order qualifies for this offerFunction. Else 0
      */
     public long calculateDiscount(Order order) {
         return Arrays.stream(participatingItems)
                 .filter(itemOnOffer -> order.hasItem(itemOnOffer))
                 .mapToLong(itemOnOffer ->
-                        longBiFunction.apply(order.getItemQuantity(itemOnOffer), itemOnOffer.getPriceInPence())).sum();
+                        offerFunction.apply(order.getItemQuantity(itemOnOffer), itemOnOffer.getPriceInPence())).sum();
     }
 
     /**
      * @param order the order to check against.
-     * @return true if the supplied order qualifies for this longBiFunction.
+     * @return true if the supplied order qualifies for this offerFunction.
      */
     public boolean appliesTo(Order order) {
         for (Item itemOnOffer : participatingItems) {
